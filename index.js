2d624b71df10c5eba475b30258c1d1d3dd23e64d
Rama322 <Koldashev.ramil@gmail.com>
2024-02-29 22:50:53 +0600

    Test

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-require('dotenv').config();
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters');
-const { mainKeyboard, specialKeyboard, inlineKeyboard, cokeKeyboard } = require('./src/mainKeyboard');
-
-const bot = new Telegraf(process.env.API_KEY)
+import allData from './Products.json' with { type: "json" };
+import "dotenv/config";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { mainKeyboard, categoryKeyboard } from "./src/mainKeyboard.js";
+const bot = new Telegraf(process.env.API_KEY);
 
 bot.command("start", (ctx) => {
-    ctx.reply('123', mainKeyboard)
-})
-
-bot.command("stop", (ctx) => {
-    ctx.reply('123', specialKeyboard)
-})
-
-bot.command("pisya", (ctx) => {
-    ctx.reply('123', inlineKeyboard)
-})
-
-bot.command("popa", (ctx) => {
-    ctx.reply('123', cokeKeyboard)
-})
+  ctx.reply("Здравствуйте, выберите команду:", mainKeyboard);
+});
 
 bot.hears("Прайс лист", (ctx) => {
-    console.log(ctx.chat.id)
-    bot.telegram.sendMessage(547170490, '123')
-})
+  ctx.reply("Категории:", categoryKeyboard);
+});
 
-bot.launch()
+//КАТЕГОРИИ
+bot.action("eCigaretts", (ctx) => {  
+    const currentCategory = allData.STROKI.find((element)=>{
+        if(element.NOMENKLATURA === 'SIGARETY ELEKTRONNYE') {
+            return true
+        }
+    })
+    const devices = currentCategory.STROKI.find((el)=>{
+        if (el.NOMENKLATURA === 'DEVAYSY'){
+            return true
+        }
+    })
+    const pods = devices.STROKI.find((el)=>{
+        if (el.NOMENKLATURA === 'POD-MODY'){
+            return true
+        }
+    })
+    pods.STROKI.forEach(element => {
+        ctx.reply(`${element.NOMENKLATURA}. В наличии: ${element.VNALICHIIOSTATOK} Шт.`)
+    });
+});
+
+bot.action("oECigaretts", (ctx) => {
+  ctx.reply("100rub");
+});
+
+bot.action("fluid", (ctx) => {
+  ctx.reply("100rub");
+});
+
+bot.action("tobacco", (ctx) => {
+  ctx.reply("100rub");
+});
+
+bot.launch();
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
--- src/mainKeyboard.js
+++ src/mainKeyboard.js
@@ -1,27 +1,18 @@
-const { Markup } = require("telegraf");
-
+import { Markup } from "telegraf";
 
 const mainKeyboard = Markup.keyboard([
-    ["Прайс лист", "Гарантийные условия"],
-    ["pidr", ":)"]
-]).resize()
+  ["Прайс лист", "Гарантийные условия"],
+]).resize();
 
+const categoryKeyboard = Markup.inlineKeyboard([
+  [
+    Markup.button.callback("Электронные сигареты", "eCigaretts"),
+    Markup.button.callback("Одноразовые сигареты", "oECigaretts"),
+  ],
+  [
+    Markup.button.callback("Жидкости", "fluid"),
+    Markup.button.callback("Табак", "tobacco"),
+  ],
+]);
 
-const specialKeyboard = Markup.keyboard([
-    Markup.button.contactRequest('Send contact'),
-    Markup.button.locationRequest('Send location')
-]).resize()
-
-
-const inlineKeyboard = Markup.inlineKeyboard([
-    Markup.button.callback('Coke', 'Coke'),
-    Markup.button.callback('Pepsi', 'popka')
-])
-
-
-const cokeKeyboard = Markup.keyboard([
-    Markup.button.callback('Coke', 'piska'),
-    Markup.button.callback('Pepsi', 'popka')
-])
-
-module.exports = { mainKeyboard, specialKeyboard, inlineKeyboard, cokeKeyboard }
+export { mainKeyboard, categoryKeyboard };
